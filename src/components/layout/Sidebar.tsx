eb830d7323adfe7086a3cb7a03778ef57b0be89f
BlesUstetu <lionustetu@gmail.com>
2026-06-12 09:09:27 +0700

    Add Sidebar component with new chat and history buttons

---
+++ src/components/layout/Sidebar.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import {
+  SquarePen,
+  History
+} from "lucide-react";
+
+interface Props {
+  onNewChat: () => void;
+  onOpenHistory: () => void;
+}
+
+export default function Sidebar({
+  onNewChat,
+  onOpenHistory
+}: Props) {
+  return (
+    <aside
+      className="
+      w-64
+      border-r
+      border-white/10
+      p-4
+      "
+    >
+      <button
+        onClick={onNewChat}
+        className="
+        flex
+        items-center
+        gap-3
+        w-full
+        p-3
+        rounded-xl
+        hover:bg-cyan-500/10
+        "
+      >
+        <SquarePen size={20} />
+        <span>Obrolan baru</span>
+      </button>
+
+      <button
+        onClick={onOpenHistory}
+        className="
+        flex
+        items-center
+        gap-3
+        w-full
+        p-3
+        rounded-xl
+        hover:bg-cyan-500/10
+        mt-2
+        "
+      >
+        <History size={20} />
+        <span>Riwayat</span>
+      </button>
+    </aside>
+  );
+}
