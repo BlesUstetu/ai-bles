3ea38bd7b6c1995ac2ba45280874fa7ec94ad5db
BlesUstetu <lionustetu@gmail.com>
2026-06-13 13:04:46 +0700

    Refactor HistoryList to use HistoryItem from store

--- src/components/history/HistoryList.tsx
+++ src/components/history/HistoryList.tsx
@@ -1,25 +1,44 @@
 "use client";
 
-import HistoryItem from "./HistoryItem";
-
-import { HistoryItem as Item } from "@/store/historyStore";
+import {
+  HistoryItem
+} from "@/store/historyStore";
 
 interface Props {
-  items: Item[];
+  items: HistoryItem[];
 }
 
 export default function HistoryList({
   items
 }: Props) {
+  if (!items.length) {
+    return (
+      <p className="text-white/50">
+        Belum ada riwayat.
+      </p>
+    );
+  }
+
   return (
-    <div className="mt-4">
+    <div className="space-y-2">
 
       {items.map((item) => (
-        <HistoryItem
+        <div
           key={item.id}
-          title={item.prompt}
-          onClick={() => {}}
-        />
+          className="
+          p-3
+          rounded-xl
+          bg-white/5
+          "
+        >
+          <p
+            className="
+            truncate
+            "
+          >
+            {item.prompt}
+          </p>
+        </div>
       ))}
 
     </div>
